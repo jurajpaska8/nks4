71124a355e94b4cb4792608c577a0b581db106cb
Juraj <juraj.paska@siemens-healthineers.com>
2021-11-13 19:12:38 +0100

    feature(aes): implement mc

--- aes.c
+++ aes.c
@@ -83,7 +83,48 @@ void transformTo1Dim(const uint8_t state2Dim[4][4], uint8_t state[16])
     }
 }
 
-int encrypt(const uint8_t data[4][4], const uint8_t sBoxData[16], const uint8_t keys[11][4][4], uint8_t dataToReturn[4][4])
+uint8_t findExp(const uint8_t a, const uint8_t multTable[16])
+{
+    for (int i = 0; i < 16; ++i) {
+        if(a == multTable[i])
+        {
+            return i;
+        }
+    }
+    return 0;
+}
+
+uint8_t mul(const uint8_t a, const uint8_t b, const uint8_t multTable[16])
+{
+    if(a==0 || b==0)
+    {
+        return 0;
+    }
+    uint8_t exp = (findExp(a, multTable) + findExp(b, multTable)) % 15;
+    return multTable[exp];
+}
+
+void mcFun(const uint8_t multTable[16], const uint8_t mattrix[4][4], const uint8_t data[4][4], uint8_t dataToReturn[4][4])
+{
+    for (int col = 0; col < 4; ++col)
+    {
+        for (int i = 0; i < 4; ++i) {
+            uint8_t tmp = 0;
+            for (int j = 0; j < 4; ++j) {
+                tmp ^= mul(mattrix[i][j], data[j][col], multTable);
+            }
+            dataToReturn[i][col] = tmp;
+        }
+    }
+
+}
+
+int encrypt(const uint8_t multipleTable[16],
+            const uint8_t mcMattrix[4][4],
+            const uint8_t data[4][4],
+            const uint8_t sBoxData[16],
+            const uint8_t keys[11][4][4],
+            uint8_t dataToReturn[4][4])
 {
     uint8_t state[4][4];
     memcpy(state, data, 16);
@@ -98,6 +139,7 @@ int encrypt(const uint8_t data[4][4], const uint8_t sBoxData[16], const uint8_t 
         // shift rows
         shiftRows(state);
         // mix columns
+        //mcFun(multipleTable, mcMattrix, state, dataToReturn);
 
         // add round key
         addRoundKey(keys[round + 1], state);
@@ -115,7 +157,12 @@ int encrypt(const uint8_t data[4][4], const uint8_t sBoxData[16], const uint8_t 
     return 1;
 }
 
-int decrypt(const uint8_t data[4][4], const uint8_t sBoxInverseData[16], const uint8_t keys[11][4][4], uint8_t dataToReturn[4][4])
+int decrypt(const uint8_t multipleTable[16],
+            const uint8_t mcMattrix[4][4],
+            const uint8_t data[4][4],
+            const uint8_t sBoxInverseData[16],
+            const uint8_t keys[11][4][4],
+            uint8_t dataToReturn[4][4])
 {
     uint8_t state[4][4];
     memcpy(state, data, 16);
@@ -156,23 +203,27 @@ void keySchedule(const uint8_t key0[4][4], const uint8_t constants[10], const ui
      */
     // last column of key0
     uint8_t lastColumn[4];
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 4; ++i)
+    {
         lastColumn[i] = key0[i][3];
     }
 
     // shift columns
     uint8_t tmp[4];
     memcpy(tmp, lastColumn, 4);
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 4; ++i)
+    {
         lastColumn[i] = tmp[(i + 1) % 4];
     }
 
     // sbox
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 4; ++i)
+    {
         lastColumn[i] = sBox[lastColumn[i]];
     }
     // affine
-    for (int i = 0; i < 4; ++i) {
+    for (int i = 0; i < 4; ++i)
+    {
         keyToReturn[i][0] = key0[i][0] ^ lastColumn[i];
         if(i == 0)
         {
@@ -188,36 +239,58 @@ void keySchedule(const uint8_t key0[4][4], const uint8_t constants[10], const ui
                 keyToReturn[row][col] = keyToReturn[row][col - 1] ^ key0[row][col];
             }
         }
-    printf("break");
 }
 
 int main()
 {
+    // S-Boxes
     const uint8_t sBox[16] = {0x5, 0xB, 0x3, 0x4, 0x1, 0x6, 0xA, 0x9, 0x7, 0xE, 0x0, 0xC, 0x2, 0xF, 0xD, 0x8};
     const uint8_t sBoxInverse[16] = {0xA, 0x4, 0xC, 0x2, 0x3, 0x0, 0x5, 0x8, 0xF, 0x7, 0x6, 0x1, 0xB, 0xE, 0x9, 0xD};
 
+    // key schedule
     const uint8_t key0[16] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
     uint8_t keys[11][4][4];
-    for (int i = 0; i < 11; ++i)
-    {
-        memcpy(keys[i], key0, 16);
+    memcpy(keys[0], key0, 16);
+
+    uint8_t constants[10] = {0x1, 0x2, 0x4, 0x8, 0x3, 0x6, 0xC, 0xB, 0x5, 0xA};
+    for (int i = 0; i < 10; ++i) {
+        keySchedule(keys[i], constants, sBox, keys[i + 1], i);
     }
+
+    // mix columns
+    const uint8_t mc[16] = {0x2, 0x3, 0x1, 0x1, 0x1, 0x2, 0x3, 0x1, 0x1, 0x1, 0x2, 0x3, 0x3, 0x1, 0x1, 0x2};
+    uint8_t mcMattrix[4][4];
+    memcpy(mcMattrix, mc, 16);
+
+    const uint8_t mcInv[16] = {0xE, 0xB, 0xD, 0x9, 0x9, 0xE, 0xB, 0xD, 0xD, 0x9, 0xE, 0xB, 0xB, 0xD, 0x9, 0xE};
+    uint8_t mcMattrixInv[4][4];
+    memcpy(mcMattrixInv, mcInv, 16);
+
+    const uint8_t multipleTable[16] = {0x1, 0x2, 0x4, 0x8, 0x3, 0x6, 0xC, 0xB, 0x5, 0xA, 0x7, 0xE, 0xF, 0xD, 0x9};
+
+
 
     uint8_t toEncrypt[16] = {0xA, 0x4, 0xC, 0x2, 0x3, 0x0, 0x5, 0x8, 0xF, 0x7, 0x6, 0x1, 0xB, 0xE, 0x9, 0xD};
     uint8_t toEnc[4][4];
     memcpy(toEnc, toEncrypt, 16);
     uint8_t encrypted[4][4];
     uint8_t decrypted[4][4];
-    encrypt(toEnc, sBox, keys, encrypted);
-    decrypt(encrypted, sBoxInverse, keys,  decrypted);
+
+    encrypt(multipleTable, mcMattrix, toEnc, sBox, keys, encrypted);
+    decrypt(multipleTable, mcMattrixInv, encrypted, sBoxInverse, keys,  decrypted);
 
     int isEqual = memcmp(toEncrypt, decrypted, 16);
     printf("is Equal= %d", isEqual);
 
-    // key schedule
-    uint8_t keyToReturn[4][4];
-    uint8_t constants[10] = {0x1, 0x2, 0x4, 0x8, 0x3, 0x6, 0xC, 0xB, 0x5, 0xA};
-    keySchedule(keys[0], constants, sBox, keys[1], 0);
+    // mc
+    uint8_t dataToReturn[4][4] = {0};
+    mcFun(multipleTable, mcMattrix, decrypted, dataToReturn);
+
+    uint8_t dataToReturnDecrypted[4][4] = {0};
+    mcFun(multipleTable, mcMattrixInv, dataToReturn, dataToReturnDecrypted);
+
+    isEqual = memcmp(decrypted, dataToReturnDecrypted, 16);
+    printf("is Equal= %d", isEqual);
 
     return 0;
 }
